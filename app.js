03488be9c84d1fff2e577e80e19e7e8ff8959914
Devendra Singh <devsinghindra@gmail.com>
2020-04-21 08:56:50 +0530

    initialized with npm and installed required packages

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server is listening on 3000");
+})
