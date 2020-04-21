d54b2ad9367ef749a9bde96d472f52aeae2bf4ee
Devendra Singh <devsinghindra@gmail.com>
2020-04-21 17:19:44 +0530

    level 5 authentication with passport and cookies for browsing session

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
-
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
-
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-});
-
-const User = new mongoose.model("User", userSchema);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+//session code
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//passport initialization and using session with that
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+    username: String,//changed schema here from email to username
+    password: String,
+});
+
+//insert the plugin in schema i.e passportlocalmongoose
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+//creating passport strategy see docs
+passport.use(User.createStrategy());
+
+//creating cookies i.e serializing
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function (req, res) {
     res.render("home");
+});
+
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function (req, res) {
+    req.logOut();
+    res.redirect("/");
 });
 
 app.get("/register", function (req, res) {
@@ -28,21 +65,15 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function (err, hashedPassword) {
+    //here instead of email username is used as it is default parameter for setting it to email you have to change schema see docs
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
         if (err) {
-            console.log("bcrypt hashing failed");
+            console.log(err);
+            res.redirect("/register");
         } else {
-            const newUser = new User({
-                email: req.body.username,
-                password: hashedPassword
-            });
-            newUser.save(function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.render("secrets");
-                }
-            });
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            })
         }
     });
 });
@@ -52,28 +83,18 @@ app.get("/login", function (req, res) {
 });
 
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({ email: username }, function (err, foundUser) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.logIn(user, function (err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser == null) {
-                res.send("Invalid email ID try again");
-            } else {
-                bcrypt.compare(password, foundUser.password, function (err, result) {
-                    // result == true
-                    if (err) {
-                        console.log("hashing failed in login bcrypt");
-                    } else {
-                        if (result == true) {
-                            res.render("secrets");
-                        } else {
-                            res.send("Invalid password try again");
-                        }
-                    }
-                });
-            }
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
         }
     });
 });
