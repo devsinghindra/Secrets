7621ed5437bd97bbadf3956b6d49197278db1a69
Devendra Singh <devsinghindra@gmail.com>
2020-04-22 13:43:06 +0530

    level 6 authentication with Oauth 2.0 with google and saving sessions

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -29,10 +33,12 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
     username: String,//changed schema here from email to username
     password: String,
+    googleId: String
 });
 
 //insert the plugin in schema i.e passportlocalmongoose
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,20 +46,53 @@ const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
 //creating cookies i.e serializing
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.get("/", function (req, res) {
     res.render("home");
 });
 
 app.get("/secrets", function (req, res) {
+    console.log(req.isAuthenticated());
     if (req.isAuthenticated()) {
         res.render("secrets");
     } else {
         res.redirect("/login");
     }
 });
+
+//i don't know the reason but put google in single quotes otherwise sessions will not save
+app.get("/auth/google",
+    passport.authenticate('google', { scope: ["profile"] }));
+
+app.get("/auth/google/secrets",
+    passport.authenticate('google', { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect secrets.
+        res.redirect("/secrets");
+    });
 
 app.get("/logout", function (req, res) {
     req.logOut();
